ffea36a55d239af8cf68c3947aa662518218f83d
DtgfjtwpXehpehp <sivuyilemtwetwe@gmail.com>
2025-08-02 10:45:57 +0200

    Redesign desktop taskbar with weather API and location

--- src/components/desktop/DesktopInterface.vue
+++ src/components/desktop/DesktopInterface.vue
@@ -69,7 +69,9 @@
     <!-- Taskbar -->
     <DesktopTaskbar
       :minimized-windows="minimizedWindows"
+      :command-center-open="false"
       @restore-window="restoreWindow"
+      @toggle-command-center="toggleCommandCenter"
     />
   </div>
 </template>
@@ -106,6 +108,11 @@ const {
   maximizeWindow,
   updateWindowPosition
 } = useWindowManagement()
+
+const toggleCommandCenter = () => {
+  // Toggle command center visibility - you can implement this logic
+  console.log('Toggle command center')
+}
 </script>
 
 <style scoped>
