6a5ba7d80b08eb05243cd29bf372bb05ee8fcebc
DtgfjtwpXehpehp <sivuyilemtwetwe@gmail.com>
2025-08-06 17:56:13 +0200

    Add View CV button to mobile social icons

--- src/components/mobile/sections/AboutSection.vue
+++ src/components/mobile/sections/AboutSection.vue
@@ -66,6 +66,14 @@
                         <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515a.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0a12.64 12.64 0 0 0-.617-1.25a.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057a19.9 19.9 0 0 0 5.993 3.03a.078.078 0 0 0 .084-.028a14.09 14.09 0 0 0 1.226-1.994a.076.076 0 0 0-.041-.106a13.107 13.107 0 0 1-1.872-.892a.077.077 0 0 1-.008-.128a10.2 10.2 0 0 0 .372-.292a.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127a12.299 12.299 0 0 1-1.873.892a.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028a19.839 19.839 0 0 0 6.002-3.03a.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.956-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.955-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.946 2.418-2.157 2.418z"/>
                       </svg>
                     </a>
+                    <li class="icon cv">
+                      <span class="tooltip">View CV</span>
+                      <a :href="document?.file_url" target="_blank" rel="noopener" v-if="document?.file_url">
+                        <svg viewBox="0 0 24 24" height="1.2em" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M18,20H6V4H13V9H18V20Z"/>
+                        </svg>
+                      </a>
+                    </li>
                   </div>
                 </div>
               </div>
@@ -97,6 +105,7 @@
 <script setup lang="ts">
 import { useAbout } from '../../../composables/useAbout'
 import { useContact } from '../../../composables/useContact'
+import { useResume } from '../../../composables/useResume'
 // import { ref, onMounted } from 'vue'
 import { defineProps, defineEmits, ref, onMounted, nextTick } from 'vue'
 import gsap from 'gsap'
@@ -106,6 +115,7 @@ import textSound from '../../../assets/sounds/textsound.mp3'
 
 const { about, fetchAbout } = useAbout()
 const { contact, fetchContact } = useContact()
+const { document, fetchDocument } = useResume()
 
 defineProps<{
   soundEnabled: boolean
@@ -184,6 +194,7 @@ onMounted(async () => {
   textAudio = new Audio(textSound)
   textAudio.preload = 'auto'
   await fetchAbout()
+  await fetchDocument()
   realName.value = about.value?.name || 'Your Real Name'
   scrambledName.value = caesarCipher(realName.value, shift)
   if (nameSpan.value) nameSpan.value.textContent = scrambledName.value
@@ -225,6 +236,7 @@ const handleSocialClick = (event: Event) => {
 onMounted(async () => {
   await fetchAbout()
   await fetchContact()
+  await fetchDocument()
 })
 </script>
 
