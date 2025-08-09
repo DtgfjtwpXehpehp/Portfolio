520ff39867ff15e708634e43cd0f1489e0890907
DtgfjtwpXehpehp <sivuyilemtwetwe@gmail.com>
2025-08-09 10:03:08 +0200

    Updated api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api'; // Adjust this port based on your backend configuration
+const API_BASE_URL = 'https://portfolio-backend-hazel-pi.vercel.app/api'; // Adjust this port based on your backend configuration
 
 const api = axios.create({
   baseURL: API_BASE_URL,
